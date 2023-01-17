56495c725d0b5edf34e3b94557b195ea10d04163
Abhijeet Salunkhe <abhijeetwebdev@gmail.com>
2023-01-18 00:41:35 +0530

    fix: naming conventions

--- main.py
+++ main.py
@@ -3,14 +3,16 @@ from services.collage_maker import CollageMaker
 
 # source image and dir
 uploads_dir = PATHS['UPLOADS']
-src_image_file = f'{uploads_dir}/mario-tiny.jpg'
+target_image = f'{uploads_dir}/mario-tiny.jpg'
+source_images = []
 
 # initiate and run collage maker
 def main():
     collage_maker = CollageMaker()
 
-    collage_maker.set_source_image(src_image_file)
-    collage_maker.read_source_image()
+    collage_maker.set_target_image(target_image)
+    collage_maker.read_target_image()
+    # collage_maker.set_source_images()
 
 if __name__=='__main__':
     main()
--- services/collage_maker.py
+++ services/collage_maker.py
@@ -4,28 +4,25 @@ from PIL import Image, ImageOps
 class CollageMaker:
 
     def __init__(self):
-        self._source_image_url = None
-        self._source_image_ref = None
-        self._pixel_matrix = None
         self._target_image_url = None
+        self._target_image_ref = None
+        self._pixel_matrix = None
+        self._source_image_urls = None
 
     # set source image link
-    def set_source_image(self, url):
-        self._source_image_url = url
-        self._source_image_ref = Image.open(url)
+    def set_target_image(self, url):
+        self._target_image_url = url
+        self._target_image_ref = Image.open(url)
         
     # get source image link
-    def get_source_image(self):
-        return self._source_image_url
+    def get_target_image(self):
+        return self._target_image_url
     
     # read source image pixels data
-    def read_source_image(self):
-        src_data = list(self._source_image_ref.getdata())
-        width, height = self._source_image_ref.size
-        print('src_data START')
-        print(src_data)
-        print('src_data END')
-        self._pixel_matrix = [src_data[i * width:(i + 1) * width] for i in range(height)]
-        print('pixel_matrix')
-        print(self._pixel_matrix)
+    def read_target_image(self):
+        image_data = list(self._target_image_ref.getdata())
+        width, height = self._target_image_ref.size
+        
+        self._pixel_matrix = [image_data[i * width:(i + 1) * width] for i in range(height)]
+        return self._pixel_matrix
 
