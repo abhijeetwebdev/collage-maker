f40556e71f979758e222a61cbf0bb9b30846a281
Abhijeet Salunkhe <abhijeetwebdev@gmail.com>
2023-01-18 01:01:39 +0530

    add: collage sample source images

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 from config import PATHS
 from services.collage_maker import CollageMaker
 
-# source image and dir
+# image sources and dirs
 uploads_dir = PATHS['UPLOADS']
 target_image = f'{uploads_dir}/mario-tiny.jpg'
-source_images = []
+source_images = [
+    f'{uploads_dir}/1.jpg',
+    f'{uploads_dir}/2.jpg',
+    f'{uploads_dir}/3.jpg',
+    f'{uploads_dir}/4.jpg',
+    f'{uploads_dir}/5.jpg'
+]
 
 # initiate and run collage maker
 def main():
     collage_maker = CollageMaker()
 
     collage_maker.set_target_image(target_image)
-    collage_maker.read_target_image()
-    # collage_maker.set_source_images()
+    collage_maker.set_source_images(source_images)
+    collage_maker.generate()
 
 if __name__=='__main__':
     main()
--- services/collage_maker.py
+++ services/collage_maker.py
@@ -9,20 +9,33 @@ class CollageMaker:
         self._pixel_matrix = None
         self._source_image_urls = None
 
+
     # set source image link
     def set_target_image(self, url):
         self._target_image_url = url
         self._target_image_ref = Image.open(url)
-        
+        self._read_target_image()
+
+
     # get source image link
     def get_target_image(self):
         return self._target_image_url
-    
+
+
     # read source image pixels data
-    def read_target_image(self):
+    def _read_target_image(self):
         image_data = list(self._target_image_ref.getdata())
         width, height = self._target_image_ref.size
         
         self._pixel_matrix = [image_data[i * width:(i + 1) * width] for i in range(height)]
         return self._pixel_matrix
 
+
+    # set source image link
+    def set_source_images(self, urls):
+        self._source_image_urls = urls
+
+    
+    def generate(self):
+        print('Start generating collage image')
+        
